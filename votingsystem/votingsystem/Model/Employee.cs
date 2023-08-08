f13c4bcfff0db4e324ffec4ba012fb2cb1a16d06
panthamarunkumar <70441456+panthamarunkumar@users.noreply.github.com>
2023-08-08 10:38:58 +0530

    Added employee model class

---
+++ votingsystem/votingsystem/Model/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace votingsystem.Model
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
